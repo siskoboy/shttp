fe42fcf3942d06b0548d1b402e28bbb7ba6e7623
Sam Hansen <solid.se7en@gmail.com>
2012-07-02 17:19:20 -0700

    Bolt on a (for now) most basic argparser

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from shttp.methods import HTTPRequest as request
 from shttp.networking import connection
 from shttp.parse import parser as reqparser
 
+from argparse import ArgumentParser
 from os import fork
 from signal import signal, SIG_IGN, SIGCHLD, SIGINT, SIGHUP
 from sys import exit
@@ -10,8 +11,17 @@ from sys import exit
 # TODO proper argparser
 # TODO sighandlers CHLD, INT, HUP
 
-socketio = connection('127.0.0.1', 8081)
-docroot = '/var/www/html'
+argparser = ArgumentParser()
+argparser.add_argument('-r', '--docroot', default='/var/www/html',
+   metavar='dir', help='Site docroot')
+argparser.add_argument('-l', '--listen', default='0.0.0.0',
+   metavar='addr', help='Address to listen on')
+argparser.add_argument('-p', '--port', default='80', type=int,
+   metavar='port', help='Port to listen on')
+
+args = argparser.parse_args()
+
+socketio = connection(args.listen, args.port)
 
 while True:
    socketio.accept()
@@ -23,7 +33,7 @@ while True:
       req = reqparser.parse(data)
 
       # build and transmit response
-      resp = response(req,docroot)
+      resp = response(req,args.docroot)
       socketio.send_response(resp)
 
       # close client connection
